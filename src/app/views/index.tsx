5c157b8f855fc08a4ebe671d42d620d795fea299
andrzejmusiol <andrzejmusiol88@gmail.com>
2025-11-27 20:49:24 +0100

    fix: Colors adjusting

--- src/app/views/index.tsx
+++ src/app/views/index.tsx
@@ -11,7 +11,7 @@ export const WeatherForecastPage = () => {
     const handleGeocodingSelect = (geocoding: Geocoding) => setSelectedGeocoding(geocoding)
 
     return (
-        <div className="h-screen w-screen flex flex-col overflow-hidden relative bg-[#0C1518]">
+        <div className="h-screen w-screen flex flex-col overflow-hidden relative bg-primary">
             <div className="z-10 flex flex-col h-full overflow-y-auto">
                 <div className="flex-shrink-0 md:flex-1 min-h-0">
                     <WeatherPage geocoding={selectedGeocoding} handleGeocodingSelect={handleGeocodingSelect} />
