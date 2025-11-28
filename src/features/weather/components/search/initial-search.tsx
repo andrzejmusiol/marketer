45b9516469cfa8441ed350d518e459f1a11e1a7e
andrzejmusiol <andrzejmusiol88@gmail.com>
2025-11-28 17:03:19 +0100

    fix: Initial screen improvement

--- src/features/weather/components/search/initial-search.tsx
+++ src/features/weather/components/search/initial-search.tsx
@@ -4,7 +4,7 @@ export const InitialSearch = () =>
     <div className="p-10 flex flex-col items-center justify-center text-center h-screen">
         <h1 className="text-5xl font-light text-white text-center">Search for location</h1>
         <p className="text-lg text-white text-center mb-10 opacity-50">or choose from recent searches</p>
-        <section className="w-1/2" aria-label="Search for city">
+        <section className="w-full md:w-1/2" aria-label="Search for city">
             <Search />
         </section>
     </div>
